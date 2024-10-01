f4d59ced41a5dc91268fa42f59510980b3444ea0
Varun Sharma <varun1598sharma@gmail.com>
2024-10-01 18:44:48 +0530

    Revert "Added DATEDIFF"

--- models/ecommerce/aggregate/purchase_patterns.sql
+++ models/ecommerce/aggregate/purchase_patterns.sql
@@ -4,7 +4,7 @@ SELECT
   purchased_at,
   added_to_cart_at,  
   returned_at,
-DATEDIFF(day, added_to_cart_at, purchased_at) AS time_to_purchase_days
+(purchased_at - added_to_cart_at) DAY(4) AS time_to_purchase_days
 FROM {{ ref('stg_purchases') }}
 
 
